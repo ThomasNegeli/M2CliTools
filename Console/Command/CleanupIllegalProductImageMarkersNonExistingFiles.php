70f564d06b7c122bbbc8dcd7b73fe8cfc48fd62c
Thomas Negeli <t.negeli@limesoda.com>
2020-08-27 12:18:08 +0200

    #12 add command to cleanup image markers referencing a non existing file

---
+++ Console/Command/CleanupIllegalProductImageMarkersNonExistingFiles.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Tnegeli\M2CliTools\Console\Command;
+
+use Magento\Catalog\Model\Indexer\Product\Flat\State;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Filesystem;
+
+
+/**
+ * Class CleanupIllegalProductImageMarkersNonExistingFiles
+ *
+ * Cleanup illegal product image markers from database for which no files are available.
+ */
+class CleanupIllegalProductImageMarkersNonExistingFiles extends Command
+{
+
+    private $resource;
+    private $filesystem;
+    private $mediaDirectory;
+    private $indexerFactory;
+
+
+    public function __construct(
+        \Magento\Indexer\Model\IndexerFactory $indexerFactory,
+        Filesystem $filesystem,
+        ResourceConnection $resource
+    )
+    {
+        $this->filesystem = $filesystem;
+        $this->resource = $resource;
+        $this->indexerFactory = $indexerFactory;
+        $this->mediaDirectory = $filesystem->getDirectoryRead(DirectoryList::MEDIA);
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('tnegeli:cleanup-illegal-product-image-markers-non-existing-files')
+            ->setDescription('Cleanup illegal product image markers from database for which no files are available.')
+            ->addOption('dry-run')
+            ->addOption('delete');
+
+        $help = "Cleanup illegal product image markers from database for which no files are available.'
+Add the --dry-run option to just get the files that are unused.";
+
+        $this->setHelp($help);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $isDryRun = $input->getOption('dry-run');
+        if (!$isDryRun) {
+            $output->writeln('WARNING: this is not a dry run. If you want to do a dry-run, add --dry-run.');
+            $question = new ConfirmationQuestion('Are you sure you want to continue? [No] ', false);
+            $this->questionHelper = $this->getHelper('question');
+            if (!$this->questionHelper->ask($input, $output, $question)) {
+                return;
+            }
+        }
+
+        $values = $this->getDbValues();
+        if (count($values) == 0) {
+            $output->writeln('You have no media gallery table entries.');
+            return;
+        }
+
+        $mediaDirectory = $this->filesystem->getDirectoryRead(DirectoryList::MEDIA);
+        $imageDir = rtrim($mediaDirectory->getAbsolutePath(),
+                "/") . DIRECTORY_SEPARATOR . 'catalog' . DIRECTORY_SEPARATOR . 'product';
+
+        $imageMarkersToRemove = array();
+        $imageMarkersToKeep = array();
+        foreach ($values as $marker) {
+            $filename = $marker['value'];
+            if (!$this->fileExists($imageDir . $filename)) {
+                $imageMarkersToRemove[$marker['value_id']] = $filename;
+                $output->writeln("The following image marker has no existing file: " . print_r($marker, true));
+            } else {
+                $imageMarkersToKeep[$marker['value_id']] = $filename;
+            }
+        }
+
+        if (count($imageMarkersToRemove) == 0) {
+            $output->writeln("There are no image markers without a file. All is fine.");
+            return;
+        }
+
+        $output->writeln("The following items are left untouched: " . print_r($imageMarkersToKeep,
+                true));
+
+        if (!$isDryRun) {
+            $coreWrite = $this->resource->getConnection('core_write');
+
+            // smaller parts to respect a large amount of images to remove
+            $imageMarkerChunksToRemove = array_chunk(array_keys($imageMarkersToRemove), 50);
+            foreach ($imageMarkerChunksToRemove as $imageMarkerChunkToRemove) {
+                $coreWrite->query($this->getImageMarkerDelete($imageMarkerChunkToRemove));
+            }
+
+            $this->reindexRequiredIndex($output);
+
+            $output->writeln("Entries where removed from products.");
+        } else {
+            $output->writeln("The following items are save to be removed: " . print_r($imageMarkersToRemove,
+                    true));
+        }
+    }
+
+    private function reindexRequiredIndex($output)
+    {
+        // catalog_product_flat is of interest to us
+        $indexerIds = array(State::INDEXER_ID);
+
+        foreach ($indexerIds as $indexerId) {
+            $indexer = $this->indexerFactory->create();
+            /* @var $indexer Indexer */
+            $indexer->load($indexerId);
+
+            if ($indexer->isScheduled()) {
+                // if we are in update by schedule mode, an index:reset is required
+                $indexer->getState()
+                    ->setStatus(\Magento\Framework\Indexer\StateInterface::STATUS_INVALID)
+                    ->save();
+                $output->writeln("The following index was marked as invalid: " . State::INDEXER_ID);
+            } else {
+                // if we are in realtime mode, we can reindex directly
+                $output->writeln("Start reindexing of: " . State::INDEXER_ID);
+                $indexer->reindexAll();
+                $output->writeln("The following index was rebuilt: " . State::INDEXER_ID);
+            }
+        }
+    }
+
+    private function getSelect()
+    {
+        $coreRead = $this->resource->getConnection('core_read');
+
+        // get the internal id of the entity type product
+        $eavEntityType = $this->resource->getConnection()->getTableName('eav_entity_type');
+        $select = 'SELECT entity_type_id FROM ' . $eavEntityType . ' WHERE entity_type_code = "catalog_product"';
+        $entityTypeId = $coreRead->fetchOne($select);
+
+        // get the relevant attribute ids
+        $eavAttribute = $this->resource->getConnection()->getTableName('eav_attribute');
+        $select = 'SELECT attribute_id FROM ' . $eavAttribute . ' WHERE entity_type_id = ' . $entityTypeId . ' AND attribute_code IN ("image","small_image","thumbnail")';
+        $imageAttributeIds = $coreRead->fetchCol($select);
+
+        // get the image marker values
+        $catalogProductEntityVarchar = $this->resource->getConnection()->getTableName('catalog_product_entity_varchar');
+        $select = 'SELECT value_id, value FROM ' . $catalogProductEntityVarchar .
+            ' WHERE attribute_id IN ("' . implode('","', $imageAttributeIds) . '") AND value != "no_selection" AND value IS NOT NULL';
+        return $select;
+    }
+
+    private function fileExists($filename)
+    {
+        return $this->mediaDirectory->isFile($filename);
+    }
+
+    private function getImageMarkerDelete($valueIds)
+    {
+        $mediaGallery = $this->resource->getConnection()->getTableName('catalog_product_entity_varchar');
+        $valueIds = implode(',', $valueIds);
+        $delete = 'DELETE FROM ' . $mediaGallery . ' WHERE value_id IN (' . $valueIds . ')';
+        return $delete;
+    }
+
+    private function getDbValues()
+    {
+        $coreRead = $this->resource->getConnection('core_read');
+        $values = $coreRead->fetchAll($this->getSelect());
+        return $values;
+    }
+
+}
--- Readme.md
+++ Readme.md
@@ -3,6 +3,7 @@
 Various CLI Tools for Magento 2
 
 ## Available commands:
+
 ```
 php bin/magento tnegeli:cleanup-unused-product-media
 ```
@@ -12,6 +13,7 @@ You can use the --dry-run option to just test the result.
 
 You can use the --delete option to remove files, instead of doing a backup.
 
+
 ```
 php bin/magento tnegeli:cleanup-unused-category-media
 ```
@@ -20,6 +22,7 @@ Use this command to backup (or delete) unused category media from filesystem.
 You can use the --dry-run option to just test the result.
 
 You can use the --delete option to remove files, instead of doing a backup.
+
 ```
 php bin/magento tnegeli:cleanup-unused-swatches-media
 ```
@@ -28,15 +31,29 @@ Use this command to backup (or delete) unused swatches media from filesystem.
 You can use the --dry-run option to just test the result.
 
 You can use the --delete option to remove files, instead of doing a backup.
+
 ```
 php bin/magento tnegeli:cleanup-illegal-product-media
 ```
 Use this command to identify and remove illegal entries in the media gallery database table, which might break catalog:images:resize process.
 
 You can use the --dry-run option to just test the result and give you a list of value_id entries from the media gallery table which are illegal.
+
 ```
 php bin/magento tnegeli:cleanup-illegal-product-media-non-existing-files
 ```
 Use this command to identify and remove illegal entries in the media gallery database table that have no files on the filesystem, which might break catalog:images:resize process.
 
 You can use the --dry-run option to just test the result and give you a list of value_id entries from the media gallery table which are illegal.
+
+```
+php bin/magento tnegeli:cleanup-illegal-product-image-markers-non-existing-files
+```
+Each product is checked for the attribute values of
+* image
+* small_image
+* thumbnail
+
+If a product references a file that does not exist, the reference is removed.
+
+You can use the --dry-run option to just test your database.
